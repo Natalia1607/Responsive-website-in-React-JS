109caaf30da06044eef3e17413f48a3a94c43dd6
Natalia Gilyazova <gilyazovan97@mail.ru>
2022-12-27 11:26:25 +0300

    Created sections Brand, Feature and Possibility

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './feature.css';
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div>
-      Feature
+    <div className='gpt3__features-container_feature'>
+      <div className='gpt3__features-container_feature-title'>
+        <div />
+        <h3>{ title }</h3>
+      </div>
+      <div className='gpt3__features-container_feature-text'>
+        <p>{ text }</p>
+      </div>
     </div>
   )
 }
